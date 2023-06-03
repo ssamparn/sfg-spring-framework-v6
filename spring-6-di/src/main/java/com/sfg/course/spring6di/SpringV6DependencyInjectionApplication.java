3c95725888ef075e209cd2823ca4cbe09250b6ad
samantrs <https://samantrs@git.rabobank.nl>
2023-06-03 19:23:34 +0200

    spring di

---
+++ spring-6-di/src/main/java/com/sfg/course/spring6di/SpringV6DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.course.spring6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringV6DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringV6DependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ spring-6-di/src/test/java/com/sfg/course/spring6di/SpringV6DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.course.spring6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringV6DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
